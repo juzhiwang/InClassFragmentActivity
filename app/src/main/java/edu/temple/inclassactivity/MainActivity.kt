fdc8a5d12370ce76cd74bf0bbcad9d11d85a0e1b
tuk91541 <juzhiwang69@gmail.com>
2023-03-16 12:20:34 -0400

    cant figurout the rest

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,23 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        val fragment = ImageDisplayFragment()
+
+        val bundle = Bundle()
+        bundle.putIntArray(IMAGES_KEY,imageArray)
+        fragment.arguments = bundle
+
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView2,fragment)
+            .commit()
+
+        findViewById<Button>(R.id.fragmentContainerView2).setOnClickListener(
+            
+        )
+
+
 
     }
 }
